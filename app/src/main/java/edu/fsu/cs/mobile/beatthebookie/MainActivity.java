f42f62218a855e71e7ad5b2b0176ce233b9ac2c5
RobertoMora10 <robertomr10@hotmail.com>
2018-12-05 21:04:24 -0500

    Login

--- app/src/main/java/edu/fsu/cs/mobile/beatthebookie/MainActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/beatthebookie/MainActivity.java
@@ -3,15 +3,41 @@ package edu.fsu.cs.mobile.beatthebookie;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseUser currentUser;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent =   new Intent(this, LoginActivity.class);
-        startActivity(intent);
 
+        auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (currentUser == null) {
+            Intent intent =   new Intent(this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
+        else
+        {
+
+        }
     }
 }
--- app/src/main/java/edu/fsu/cs/mobile/beatthebookie/RegisterActivity.java
+++ app/src/main/java/edu/fsu/cs/mobile/beatthebookie/RegisterActivity.java
@@ -1,13 +1,84 @@
 package edu.fsu.cs.mobile.beatthebookie;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText mUser;
+    EditText mPassword;
+    EditText mConfirm;
+    Button button;
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mUser=(EditText) findViewById(R.id.RUsernameEdit);
+        mPassword=(EditText) findViewById(R.id.RpasswordEdit);
+        mConfirm=(EditText) findViewById(R.id.ConfirmEdit);
+        button=(Button) findViewById(R.id.button);
+        mAuth=FirebaseAuth.getInstance();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view)
+            {
+                CreateAccount();
+
+            }
+        });
+
+    }
+
+    private void CreateAccount()
+    {
+        String email = mUser.getText().toString();
+        String password = mPassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+            Toast.makeText(this,"Enter a email", Toast.LENGTH_SHORT).show();
+
+        if(TextUtils.isEmpty(password))
+            Toast.makeText(this,"Enter a password", Toast.LENGTH_SHORT).show();
+
+        else
+        {
+            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                {
+                    if(task.isSuccessful())
+                    {
+                         Toast.makeText(RegisterActivity.this,"Success!",Toast.LENGTH_LONG).show();
+                         Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                         startActivity(intent);
+                    }
+
+                    else
+                    {
+                        String message = task.getException().toString();
+                        Toast.makeText(RegisterActivity.this,"Error: " + message,Toast.LENGTH_LONG).show();
+
+                    }
+
+                }
+            });
+        }
     }
 }
